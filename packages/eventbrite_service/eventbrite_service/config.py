bb7636d9421f3ebe869153c286aa6054ab433ea0
“sajeremy” <9jsantiago4@gmail.com>
2024-12-21 22:36:05 -0500

    style: sort imports

--- packages/eventbrite_service/eventbrite_service/config.py
+++ packages/eventbrite_service/eventbrite_service/config.py
@@ -1,5 +1,6 @@
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
 
 load_dotenv()
 api_key = os.getenv("EVENTBRITE_API_KEY")
@@ -13,12 +14,12 @@ eventbrite_api_base_url = "https://www.eventbriteapi.com/v3/"
 nyc_organizers = {
     "bachatamania": "https://www.eventbrite.com/o/bachatamania-97398257463",
     "empire_mambo": "https://www.eventbrite.com/o/empire-dance-studio-nyc-90490307953",
-    "dj_john_john": "https://www.eventbrite.com/o/dj-john-john-12626967351"
+    "dj_john_john": "https://www.eventbrite.com/o/dj-john-john-12626967351",
 }
 
 reoccurring_events = {
     "taj_mondays": "https://www.eventbrite.com/e/latin-mondays-at-taj-lounge-fall-specials-2024-tickets-472995290647",
-    "solas_thursdays": "https://www.eventbrite.com/e/passion-thursdays-at-solas-bar-east-village-tickets-976971376687"
+    "solas_thursdays": "https://www.eventbrite.com/e/passion-thursdays-at-solas-bar-east-village-tickets-976971376687",
 }
 
 taj_event_example = {
--- packages/eventbrite_service/eventbrite_service/events.py
+++ packages/eventbrite_service/eventbrite_service/events.py
@@ -1,16 +1,17 @@
-import requests
 import json
-import time
 import random
-import eventbrite_service.config as config
+import time
+
+import requests
 from bs4 import BeautifulSoup
-from loguru import logger
-from selenium  import webdriver
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+
+import eventbrite_service.config as config
 
 
 def get_future_event_ids(organizer_url: str) -> list[str]:
-    """ Get event ids for future events
+    """Get event ids for future events
 
     Args:
         organizer_url (str): URL of the organizer's eventbrite page
@@ -27,22 +28,23 @@ def get_future_event_ids(organizer_url: str) -> list[str]:
 
     rendered_html = driver.page_source
 
-    soup = BeautifulSoup(rendered_html, 'html.parser')
+    soup = BeautifulSoup(rendered_html, "html.parser")
     driver.quit()
 
-    parent_divs = soup.find_all('div', attrs={config.parent_div_attr_key: config.parent_div_attr_val})
+    parent_divs = soup.find_all("div", attrs={config.parent_div_attr_key: config.parent_div_attr_val})
     event_ids = set()
 
     for div in parent_divs:
-        a_elements = div.find_all('a', href=True, attrs={config.a_tag_attr_key: True})
+        a_elements = div.find_all("a", href=True, attrs={config.a_tag_attr_key: True})
         for a_element in a_elements:
-            event_id = a_element['data-event-id']
+            event_id = a_element["data-event-id"]
             event_ids.add(event_id)
 
     # Convert the set to a list
     unique_event_ids = list(event_ids)
 
     return unique_event_ids
+
 
 def get_event_details(event_id: str) -> dict:
     """Get event details for a given event id
@@ -57,7 +59,7 @@ def get_event_details(event_id: str) -> dict:
     url = f"{config.eventbrite_api_base_url}events/{event_id}/"
     headers = {"Authorization": f"Bearer {config.secret_token}"}
 
-    jitter = random.uniform(1,9)
+    jitter = random.uniform(1, 9)
     time.sleep(jitter)
 
     response = requests.get(url, headers=headers)
@@ -73,4 +75,3 @@ print(event_ids)
 
 for event_id in event_ids:
     print(get_event_details(event_id))
-
